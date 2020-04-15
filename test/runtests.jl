7954179207971ea62c5744aff400ea54b59116d2
Julian P Samaroo <jpsamaroo@jpsamaroo.me>
2020-04-14 21:14:41 -0500

    fixup! Add on-device tests with AMDGPUnative.jl

--- test/runtests.jl
+++ test/runtests.jl
@@ -20,16 +20,3 @@ if HSARuntime.configured
 else
     error("HSARuntime.jl has not been configured; skipping all tests")
 end
-
-#=
-queue = HSAQueue(agent)
-kernel = HSAKernel(agent, executable, "&__vector_copy_kernel")
-inp = zeros(HSAArray{UInt32}, 1024, 1024)
-out = ones(HSAArray{UInt32}, 1024, 1024)
-args = HSAKernelArgs(inp, out)
-signal = HSASignal()
-launch(queue, kernel, args, signal;
-       workgroup_size=(256,1,1),
-       grid_size=(1024*1024,1,1))
-wait(signal)
-=#
